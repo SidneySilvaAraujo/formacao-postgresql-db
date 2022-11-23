6e1b50d882047789492d834977a3dc2feb6e0cdf
SidneySilvaAraujo <sidneiaraujo219@gmail.com>
2022-11-23 08:26:37 -0300

    inserindo dados na tabela

---
+++ curso-basico-postgresql/inserindoDados.sql
@@ -0,0 +1,23 @@
+INSERT INTO aluno (
+    nome,
+    cpf,
+    observacao,
+    idade,
+    dinheiro,
+    altura,
+    ativo,
+    data_nascimento,
+    hora_aula,
+    matriculado_em
+) VALUES(
+'andré',
+'12345678901',
+'observação sobre o aluno',
+38,
+1000.0,
+1.75,
+TRUE,
+'1930-05-12',
+'15:20:00',
+'2004-12-01 10:15:40'
+);
